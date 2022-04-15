5f7189231d3a2df44b735326817448ed22917172
HaritSharma-prog <haritsharma058@gmail.com>
2022-04-15 22:54:13 +0530

    Added my project

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ['./public/**/*.{html,hs}'],
+  theme: {
+    extend: {
+      minWidth:{
+        '13':'13px',
+        "260":"260px",
+      },
+      minHeight:{
+        "11":"11px",
+        "150px":"150px",
+      },
+      fontFamily:{
+        "DM Serif Display":[ 'DM Serif Display', 'serif'],
+        "Frank Ruhl Libre": ['Frank Ruhl Libre', 'serif'],
+        "League Spartan": ['League Spartan', 'sans-serif'],
+        "Poppin": ['Poppins', 'sans-serif'],
+      },
+      colors:{
+        "sky-450":"#30C2EF",
+      },
+      width:
+      {
+        "1/7":"71.35%"
+      }
+    },
+  },
+  plugins: [],
+}
